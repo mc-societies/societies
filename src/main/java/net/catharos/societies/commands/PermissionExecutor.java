9ce4b472ffcdc798d37c45c9daac2e2611ae7956
p000ison <maximilian.ammann@googlemail.com>
2014-08-13 22:52:59 +0200

    Added shading

---
+++ src/main/java/net/catharos/societies/commands/PermissionExecutor.java
@@ -0,0 +1,7 @@
+package net.catharos.societies.commands;
+
+/**
+ * Created by max on 04.08.14.
+ */
+public class PermissionExecutor {
+}
---
+++ src/main/java/net/catharos/societies/commands/RequestExecutor.java
@@ -0,0 +1,6 @@
+package net.catharos.societies.commands;
+
+import net.catharos.lib.core.command.Executor;
+import net.catharos.lib.core.command.sender.Sender;
+
+
