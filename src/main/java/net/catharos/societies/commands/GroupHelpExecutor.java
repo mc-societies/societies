386d20774fa7289e262abd4164a8868d8709a1e4
p000ison <maximilian.ammann@googlemail.com>
2014-08-29 15:17:55 +0200

    Print command structure

--- src/main/java/net/catharos/societies/commands/GroupHelpExecutor.java
+++ src/main/java/net/catharos/societies/commands/GroupHelpExecutor.java
@@ -4,6 +4,7 @@ import net.catharos.lib.core.command.*;
 import net.catharos.lib.core.command.sender.Sender;
 import org.bukkit.ChatColor;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -15,25 +16,40 @@ class GroupHelpExecutor<S extends Sender> implements Executor<S> {
     public void execute(CommandContext<S> ctx, S sender) {
         GroupCommand<S> command = (GroupCommand<S>) ctx.getCommand();
 
+        LinkedList<Command> pre = new LinkedList<Command>();
+        pre.push(command);
+        execute(command, sender, pre);
+    }
 
+    public void execute(GroupCommand<S> command, S sender, LinkedList<Command> pre) {
         for (Command<S> cmd : command.getChildren()) {
             StringBuilder builder = new StringBuilder();
             if (cmd instanceof ExecutableCommand) {
                 ExecutableCommand executableCommand = (ExecutableCommand) cmd;
 
-                append(builder, executableCommand);
+                append(builder, executableCommand, pre);
                 appendDesc(builder, cmd);
             } else {
-                append(builder, cmd);
+                append(builder, cmd, pre);
                 appendDesc(builder, cmd);
+                pre.push(cmd);
+                execute(((GroupCommand<S>) cmd), sender, pre);
+                pre.pop();
             }
 
             sender.send(builder.toString());
         }
     }
 
-    private StringBuilder append(StringBuilder builder, Command<?> command) {
-        builder.append(ChatColor.AQUA).append("  /").append(command.getIdentifier());
+    private StringBuilder append(StringBuilder builder, Command<?> command, LinkedList<Command> pre) {
+        builder.append(ChatColor.AQUA).append("  /");
+
+
+        for (Command cmd : pre) {
+            builder.append(cmd.getIdentifier()).append(' ');
+        }
+
+        builder.append(command.getIdentifier());
         return builder;
 
     }
@@ -44,8 +60,8 @@ class GroupHelpExecutor<S extends Sender> implements Executor<S> {
     }
 
 
-    private StringBuilder append(StringBuilder builder, ExecutableCommand<?> command) {
-        append(builder, (Command<?>) command);
+    private StringBuilder append(StringBuilder builder, ExecutableCommand<?> command, LinkedList<Command> pre) {
+        append(builder, (Command<?>) command, pre);
 
         List<Argument> arguments = command.getArguments();
 
