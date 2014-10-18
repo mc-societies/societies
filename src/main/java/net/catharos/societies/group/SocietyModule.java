cfeac4e8f1d91bae3876d7d65c4e9b62b3bf0129
p000ison <maximilian.ammann@googlemail.com>
2014-10-18 20:59:17 +0200

    Started work on default ranks

--- src/main/java/net/catharos/societies/group/SocietyModule.java
+++ src/main/java/net/catharos/societies/group/SocietyModule.java
@@ -1,13 +1,19 @@
 package net.catharos.societies.group;
 
+import com.google.inject.TypeLiteral;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
+import com.google.inject.multibindings.Multibinder;
+import com.google.inject.name.Names;
 import net.catharos.groups.*;
 import net.catharos.groups.rank.DefaultRank;
 import net.catharos.groups.rank.Rank;
 import net.catharos.groups.rank.RankFactory;
+import net.catharos.groups.rank.StaticRankProvider;
 import net.catharos.groups.validate.NameValidator;
 import net.catharos.groups.validate.TagValidator;
 import net.catharos.lib.shank.AbstractModule;
+
+import java.util.UUID;
 
 /**
  * Represents a SocietyModule
@@ -33,5 +39,17 @@ public class SocietyModule extends AbstractModule {
 
         bind(NameValidator.class).to(SimpleNameValidator.class);
         bind(TagValidator.class).to(SimpleTagValidator.class);
+
+
+        //fixme add rules
+        defaultRanks().addBinding().toInstance(new StaticRankProvider(UUID.randomUUID(), "Leader", 0));
+        defaultRanks().addBinding().toInstance(new StaticRankProvider(UUID.randomUUID(), "Member", 0));
     }
+
+    public Multibinder<StaticRankProvider> defaultRanks() {
+        return Multibinder
+                .newSetBinder(binder(), new TypeLiteral<StaticRankProvider>() {}, Names.named("default-ranks"))
+                .permitDuplicates();
+    }
+
 }
