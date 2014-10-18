3b67ae7ee0faa460c243288d08d8e63cf0fd487f
p000ison <maximilian.ammann@googlemail.com>
2014-10-18 21:20:41 +0200

    Added hasRule()

--- src/main/java/net/catharos/societies/commands/RuleStep.java
+++ src/main/java/net/catharos/societies/commands/RuleStep.java
@@ -1,14 +1,10 @@
 package net.catharos.societies.commands;
 
-import com.google.inject.Inject;
 import net.catharos.groups.Member;
-import net.catharos.groups.setting.Setting;
 import net.catharos.lib.core.command.CommandContext;
 import net.catharos.lib.core.command.ExecuteException;
 import net.catharos.lib.core.command.Executor;
 import net.catharos.lib.core.command.sender.Sender;
-
-import java.util.Map;
 
 /**
  * Represents a RuleStep
@@ -16,11 +12,6 @@ import java.util.Map;
 public class RuleStep implements Executor<Sender> {
 
     public static final String RULE = "rule";
-
-    private final Map<String, Setting> rules;
-
-    @Inject
-    public RuleStep(Map<String, Setting> rules) {this.rules = rules;}
 
     @Override
     public void execute(CommandContext<Sender> ctx, Sender sender) throws ExecuteException {
@@ -32,20 +23,7 @@ public class RuleStep implements Executor<Sender> {
 
         String rule = ctx.getCommand().get(RULE);
 
-        if (rule == null) {
-            return;
-        }
-
-        Setting setting = rules.get(rule);
-
-        if (setting == null) {
-            ctx.cancel();
-            throw new ExecuteException("No setting for rule " + rule + " found!", ctx);
-        }
-
-        Object result = member.get(setting);
-
-        if (result == null) {
+        if (!member.hasRule(rule)) {
             sender.send("no-rule");
             ctx.cancel();
         }
