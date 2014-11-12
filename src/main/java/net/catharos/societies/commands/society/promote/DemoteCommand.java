6e74756102258ae0acb2358e931a5765fcf65b06
p000ison <maximilian.ammann@googlemail.com>
2014-11-12 21:50:07 +0100

    Added demote/promote command

---
+++ src/main/java/net/catharos/societies/commands/society/promote/DemoteCommand.java
@@ -0,0 +1,41 @@
+package net.catharos.societies.commands.society.promote;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import net.catharos.groups.Member;
+import net.catharos.groups.rank.Rank;
+import net.catharos.lib.core.command.CommandContext;
+import net.catharos.lib.core.command.Executor;
+import net.catharos.lib.core.command.reflect.*;
+import net.catharos.societies.commands.RuleStep;
+
+/**
+ * Represents a RelationListCommand
+ */
+@Command(identifier = "command.demote")
+@Permission("societies.demote")
+@Meta(@Entry(key = RuleStep.RULE, value = "demote"))
+@Sender(Member.class)
+public class DemoteCommand implements Executor<Member> {
+
+    @Option(name = "argument.target.member")
+    Member target;
+
+    private final Rank normalDefaultRank;
+
+    @Inject
+    public DemoteCommand(@Named("super-default-rank") Rank normalDefaultRank) {
+        this.normalDefaultRank = normalDefaultRank;
+    }
+
+    @Override
+    public void execute(CommandContext<Member> ctx, Member sender) {
+        if (!target.hasRank(normalDefaultRank)) {
+            sender.send("target-member.not-promoted", target.getName());
+            return;
+        }
+
+        target.removeRank(normalDefaultRank);
+        sender.send("target-member.demoted", target.getName());
+    }
+}
---
+++ src/main/java/net/catharos/societies/commands/society/promote/PromoteCommand.java
@@ -0,0 +1,41 @@
+package net.catharos.societies.commands.society.promote;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import net.catharos.groups.Member;
+import net.catharos.groups.rank.Rank;
+import net.catharos.lib.core.command.CommandContext;
+import net.catharos.lib.core.command.Executor;
+import net.catharos.lib.core.command.reflect.*;
+import net.catharos.societies.commands.RuleStep;
+
+/**
+ * Represents a RelationListCommand
+ */
+@Command(identifier = "command.promote")
+@Permission("societies.promote")
+@Meta(@Entry(key = RuleStep.RULE, value = "promote"))
+@Sender(Member.class)
+public class PromoteCommand implements Executor<Member> {
+
+    @Option(name = "argument.target.member")
+    Member target;
+
+    private final Rank normalDefaultRank;
+
+    @Inject
+    public PromoteCommand(@Named("super-default-rank") Rank normalDefaultRank) {
+        this.normalDefaultRank = normalDefaultRank;
+    }
+
+    @Override
+    public void execute(CommandContext<Member> ctx, Member sender) {
+        if (target.hasRank(normalDefaultRank)) {
+            sender.send("target-member.already-promoted", target.getName());
+            return;
+        }
+
+        target.addRank(normalDefaultRank);
+        sender.send("target-member.promoted", target.getName());
+    }
+}
