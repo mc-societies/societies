5c7370469772d3b8c2cead56298334ffb7d0e783
p000ison <maximilian.ammann@googlemail.com>
2015-01-31 22:35:38 +0100

    Updated sieging

--- src/main/java/org/societies/sieging/sql/SQLSiegeController.java
+++ src/main/java/org/societies/sieging/sql/SQLSiegeController.java
@@ -1,7 +1,5 @@
 package org.societies.sieging.sql;
 
-import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.ListeningExecutorService;
 import gnu.trove.set.hash.THashSet;
 import org.joda.time.DateTime;
 import org.jooq.Delete;
@@ -17,7 +15,6 @@ import org.societies.groups.group.GroupProvider;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.Callable;
 
 /**
  * Represents a SQLSiegeController
@@ -32,63 +29,54 @@ class SQLSiegeController implements SiegeController {
     private final CityProvider cityProvider;
 
     private final SiegingQueries queries;
-    private final ListeningExecutorService service;
 
     SQLSiegeController(SiegeFactory siegeFactory,
                        Map<UUID, Wager> wagers,
                        GroupProvider groupProvider, CityProvider cityProvider,
-                       SiegingQueries queries, ListeningExecutorService service) {
+                       SiegingQueries queries) {
         this.queries = queries;
         this.siegeFactory = siegeFactory;
         this.wagers = wagers;
         this.groupProvider = groupProvider;
         this.cityProvider = cityProvider;
-        this.service = service;
     }
 
-    public ListenableFuture<Siege> start(final Siege siege) {
-        return service.submit(new Callable<Siege>() {
-            @Override
-            public Siege call() throws Exception {
-                Insert query = queries.getQuery(SiegingQueries.INSERT_SIEGE);
+    public Siege start(final Siege siege) {
 
-                Location location = siege.getLocationInitiated();
+        Insert query = queries.getQuery(SiegingQueries.INSERT_SIEGE);
 
-                query.bind("uuid", siege.getUUID());
-                query.bind("city", siege.getCity().getUUID());
-                query.bind("society", siege.getBesieger().getGroup().getUUID());
-                query.bind("x", (short) location.getRoundedX());
-                query.bind("y", (short) location.getRoundedY());
-                query.bind("z", (short) location.getRoundedZ());
-                query.bind("wager", siege.getWager().getUUID());
+        Location location = siege.getLocationInitiated();
 
-                query.execute();
+        query.bind("uuid", siege.getUUID());
+        query.bind("city", siege.getCity().getUUID());
+        query.bind("society", siege.getBesieger().getGroup().getUUID());
+        query.bind("x", (short) location.getRoundedX());
+        query.bind("y", (short) location.getRoundedY());
+        query.bind("z", (short) location.getRoundedZ());
+        query.bind("wager", siege.getWager().getUUID());
 
-                return siege;
-            }
-        });
+        query.execute();
+
+        return siege;
+
     }
 
     @Override
-    public ListenableFuture<Siege> start(Group group, City city, Location location) {
+    public Siege start(Group group, City city, Location location) {
         return start(siegeFactory.create(group, city, location));
     }
 
     @Override
-    public ListenableFuture<Siege> start(Group group, City city, Location location, Wager wager) {
+    public Siege start(Group group, City city, Location location, Wager wager) {
         return start(siegeFactory.create(group, city, location, wager));
     }
 
     @Override
-    public ListenableFuture<Siege> getSiege(final UUID uuid) {
-        return service.submit(new Callable<Siege>() {
-            @Override
-            public Siege call() throws Exception {
-                Select<SiegesRecord> query = queries.getQuery(SiegingQueries.SELECT_SIEGE_BY_SIEGE);
-                query.bind("uuid", uuid);
-                return newSiege(query.fetchOne());
-            }
-        });
+    public Siege getSiege(final UUID uuid) {
+
+        Select<SiegesRecord> query = queries.getQuery(SiegingQueries.SELECT_SIEGE_BY_SIEGE);
+        query.bind("uuid", uuid);
+        return newSiege(query.fetchOne());
     }
 
     private Siege newSiege(SiegesRecord record) {
@@ -117,60 +105,46 @@ class SQLSiegeController implements SiegeController {
     }
 
     @Override
-    public ListenableFuture<Set<Siege>> getSieges(final City city) {
-        return service.submit(new Callable<Set<Siege>>() {
-            @Override
-            public Set<Siege> call() throws Exception {
-                Select<SiegesRecord> query = queries.getQuery(SiegingQueries.SELECT_SIEGES_BY_CITY);
-                query.bind("city", city.getUUID());
+    public Set<Siege> getSieges(final City city) {
+        Select<SiegesRecord> query = queries.getQuery(SiegingQueries.SELECT_SIEGES_BY_CITY);
+        query.bind("city", city.getUUID());
 
-                return getSieges(query);
-            }
-        });
-
+        return getSieges(query);
     }
 
     @Override
-    public ListenableFuture<Set<Siege>> getSieges(final Group besieger) {
-        return service.submit(new Callable<Set<Siege>>() {
-            @Override
-            public Set<Siege> call() throws Exception {
-                Select<SiegesRecord> query = queries.getQuery(SiegingQueries.SELECT_SIEGES_BY_BESIEGER);
-                query.bind("besieger", besieger.getUUID());
+    public Set<Siege> getSieges(final Group besieger) {
 
-                return getSieges(query);
-            }
-        });
+        Select<SiegesRecord> query = queries.getQuery(SiegingQueries.SELECT_SIEGES_BY_BESIEGER);
+        query.bind("besieger", besieger.getUUID());
+
+        return getSieges(query);
     }
 
     @Override
-    public ListenableFuture<Set<Siege>> getSiegesAgainst(final Group victim) {
-        return service.submit(new Callable<Set<Siege>>() {
-            @Override
-            public Set<Siege> call() throws Exception {
-                THashSet<Siege> sieges = new THashSet<Siege>();
+    public Set<Siege> getSiegesAgainst(final Group victim) {
 
-                Select<Record8<UUID, UUID, UUID, Short, Short, Short, UUID, DateTime>> query = queries
-                        .getQuery(SiegingQueries.SELECT_SIEGES_BY_SIEGED);
+        THashSet<Siege> sieges = new THashSet<Siege>();
 
-                query.bind("sieged", victim.getUUID());
+        Select<Record8<UUID, UUID, UUID, Short, Short, Short, UUID, DateTime>> query = queries
+                .getQuery(SiegingQueries.SELECT_SIEGES_BY_SIEGED);
 
-                for (Record8<UUID, UUID, UUID, Short, Short, Short, UUID, DateTime> record : query.fetch()) {
-                    Wager wager = wagers.get(record.value7());
-                    Location location = new Location(null, record.value4(), record.value5(), record.value6());
+        query.bind("sieged", victim.getUUID());
 
-                    Group group = groupProvider.getGroup(record.value2());
-                    City city = cityProvider.getCity(record.value3());
+        for (Record8<UUID, UUID, UUID, Short, Short, Short, UUID, DateTime> record : query.fetch()) {
+            Wager wager = wagers.get(record.value7());
+            Location location = new Location(null, record.value4(), record.value5(), record.value6());
 
-                    sieges.add(new SQLSiege(
-                            record.value1(),
-                            group.get(Besieger.class), city,
-                            location, record.value8(), wager));
-                }
+            Group group = groupProvider.getGroup(record.value2());
+            City city = cityProvider.getCity(record.value3());
 
-                return sieges;
-            }
-        });
+            sieges.add(new SQLSiege(
+                    record.value1(),
+                    group.get(Besieger.class), city,
+                    location, record.value8(), wager));
+        }
+
+        return sieges;
     }
 
     @Override
