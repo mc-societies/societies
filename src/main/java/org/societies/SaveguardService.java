eef4af4a5b811a9373c18a67169c233ab2b564cf
p000ison <maximilian.ammann@googlemail.com>
2015-03-18 18:36:25 +0100

    Simplify check

--- src/main/java/org/societies/SaveguardService.java
+++ src/main/java/org/societies/SaveguardService.java
@@ -46,7 +46,7 @@ public class SaveguardService extends AbstractService {
         boolean newBackup = true;
 
         for (String file : backups) {
-            if (versionCompare(FilenameUtils.getBaseName(file), version) == 0) {
+            if (FilenameUtils.getBaseName(file).equalsIgnoreCase(version)) {
                 newBackup = false;
             }
         }
@@ -54,30 +54,6 @@ public class SaveguardService extends AbstractService {
         if (newBackup) {
             File file = new File(backup, version + ".zip");
             saveguard.backup(FileUtils.openOutputStream(file));
-        }
-    }
-
-    public int versionCompare(String str1, String str2)
-    {
-        String[] vals1 = str1.split("\\.");
-        String[] vals2 = str2.split("\\.");
-        int i = 0;
-        // set index to first non-equal ordinal or length of shortest version string
-        while (i < vals1.length && i < vals2.length && vals1[i].equals(vals2[i]))
-        {
-            i++;
-        }
-        // compare first non-equal ordinal number
-        if (i < vals1.length && i < vals2.length)
-        {
-            int diff = Integer.valueOf(vals1[i]).compareTo(Integer.valueOf(vals2[i]));
-            return Integer.signum(diff);
-        }
-        // the strings are equal or one string is a substring of the other
-        // e.g. "1.2.3" = "1.2.3" or "1.2.3" < "1.2.3.4"
-        else
-        {
-            return Integer.signum(vals1.length - vals2.length);
         }
     }
 }
